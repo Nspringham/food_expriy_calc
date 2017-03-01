54029e5e972244b07a24f60e5a9d4fbd874f123d
Nick S <nspringham@aol.co.uk>
2017-03-01 13:46:59 -0800

    removed some code that was not needed

---
+++ README.txt
@@ -0,0 +1,1 @@
+A small program to work out the expiry date of a product and print it to console.
--- food_calc.py
+++ food_calc.py
@@ -1,5 +1,5 @@
 #! python3
-
+# A small program to work out the expiry date of a product and print it to console.
 
 import datetime
 
@@ -7,27 +7,20 @@ import datetime
 today = datetime.date.today()
 print(today.strftime('Today\'s date: %d, %b %Y\n'))
 
-# Dictionary containing food keys and their corresponding values
-food = {'fudge_cake': 3,
-        'fruit_coulis': 10,
-        'GF_brownie': 3,
-        'Honeycomb': 4,
-        'Lemon_poss': 4,
-        'Profiterol': 2,
-        'Tiramisu': 3,
-        'Vanilla_Cake': 3}
 
-fudge = food['fudge_cake']
-coulis = food['fruit_coulis']
-brownie = food['GF_brownie']
-honey = food['Honeycomb']
-posset = food['Lemon_poss']
-prof = food['Profiterol']
-tiramisu = food['Tiramisu']
-cheesecake = food['Vanilla_Cake']
+# food variables and their corresponding values
+foods = dict(
+    fudge = 3,
+    coulis = 10,
+    brownie = 3,
+    honey = 4,
+    posset = 4,
+    prof = 2,
+    tiramisu = 3,
+    cheesecake = 3
+    )
 
-
-
+# Using food dictionary values with time delta method to aquire expiry date.
 def expiry_calc(item_name):
 
     today = datetime.date.today()
@@ -35,33 +28,20 @@ def expiry_calc(item_name):
     newDay = today + aditionalDay
     print(newDay.strftime('--Expiry: %a, %d/%m/%Y--'))
 
+
+
 # Calling the expiry() function with the 'variable' keyword e.g. expiry_calc(brownie) will output correctly.
 def user_select():
     keyword_set = {'brownie', 'cheesecake', 'coulis', 'fudge', 'honey', 'prof', 'posset', 'tiramisu'}
-    print(keyword_set)
+    print(", ".join(foods))
     while True:
         user_input = input('\nPlease enter food item:\n')
         if user_input == "help":
-            print(keyword_set)
+            print(", ".join(foods))
         elif user_input not in keyword_set:
             print('entry incorrect')
-        elif user_input == "brownie":
-            expiry_calc(brownie)
-        elif user_input == "fudge":
-            expiry_calc(fudge)
-        elif user_input == "coulis":
-            expiry_calc(coulis)
-        elif user_input == "honey":
-            expiry_calc(honey)
-        elif user_input == "posset":
-            expiry_calc(posset)
-        elif user_input == "tiramisu":
-            expiry_calc(tiramisu)
-        elif user_input == "cheesecake":
-            expiry_calc(cheesecake)
-        elif user_input == "prof":
-            expiry_calc(prof)
         else:
-            print('error occured')
+            expiry_calc(foods[user_input])
 
-user_select()
+if __name__ == '__main__':
+    user_select()
